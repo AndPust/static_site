6ceb784b7cb538fe7c099f3bc4d227bf29e42c8b
Andrzej Pustelnik <andrzej.pustelnik@student.uj.edu.pl>
2024-10-03 22:14:33 +0200

    Main script clears public and copies static content

--- src/main.py
+++ src/main.py
@@ -1,6 +1,39 @@
 print("hello world")
 
 from textnode import TextNode
+import os, shutil
+
+def clean_directory(dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+        return
+    
+    shutil.rmtree(dir)
+
+    os.mkdir(dir)
+
+
+def rec_copy(from_dir, to_dir):
+    if not os.path.exists(from_dir):
+        raise RuntimeError("Directory to be copied from doesn't exist")
+    if not os.path.exists(to_dir):
+        raise RuntimeError("Directory to be copied to doesn't exist")
+    
+    for f in os.listdir(from_dir):
+        p = os.path.join(from_dir, f)
+        if os.path.isfile(p):
+            print(f"Copy {f} from {p} to {to_dir}")
+            shutil.copy(p, to_dir)
+        elif os.path.isdir(p):
+            d = os.path.join(to_dir, f)
+            old_d = os.path.join(from_dir, f)
+            print(f"Making dir {d}")
+            os.mkdir(d)
+            print(f"Calling rec_copy from {old_d} to {d}")
+            rec_copy(old_d, d)
+        else:
+            raise RuntimeError(f"{p} is neither a file nor a directory?")
+        
 
 
 def main():
@@ -8,6 +41,11 @@ def main():
     n = TextNode("Abcd", "", "http://tarnow.pl")
 
     print(n)
+
+    clean_directory("./public")
+    rec_copy("./static", "./public")
+
+
 
 if __name__ == "__main__":
     main()
